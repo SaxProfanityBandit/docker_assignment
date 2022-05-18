ecc1091a3194082057ec0276518fda5d4dc6b0f4
Rickard Sahlgren <rsahlgren@gmail.com>
2022-05-18 11:10:02 +0200

    More progress on mysql image

--- mysql/init.sql
+++ mysql/init.sql
@@ -2,3 +2,14 @@ CREATE USER docker;
 CREATE DATABASE docker;
 GRANT ALL PRIVILEGES ON *.* TO 'docker'@'%';
 FLUSH PRIVILEGES;
+
+CREATE TABLE persons (
+    PersonID int NOT NULL AUTO_INCREMENT,
+    Name varchar(255),
+    Age int
+    PRIMARY KEY (PersonID)
+);
+
+INSERT INTO persons (Name, Age) values (
+    "Rickard", 28
+);
